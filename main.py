ff34be0abb50b29c19b63481e4ed1e328475a86c
stephen.easley-walsh <stephen.easley-walsh@studygroup.com>
2025-08-19 20:09:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+x = int(input('Give me a number more than 0 but less than 10: '))
+
+if x > 0:
+    if x < 10:
+        print("Thank you")
+    else:
+        print("That is too big")
+else:
+    print("That is too small")
